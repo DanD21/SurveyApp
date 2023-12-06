acd62630cc7b9326c6c92812b00fcd00d0d321e6
Dan Danilescu <dan.danilescu@gmail.com>
2023-12-06 17:31:34 +0000

    Initial Commit

---
+++ SurveyApp/SurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyAppApp.swift
+//  SurveyApp
+//
+//  Created by Dan Danilescu on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
